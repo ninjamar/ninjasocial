b5fa1e46a02fdc6cd8a5a1ddfa073d79a43f4005
ninjamar <70116773+ninjamar@users.noreply.github.com>
2023-02-23 09:21:56 +0800

    temp fix for email verification

--- api_action.py
+++ api_action.py
@@ -9,8 +9,9 @@ def get_new_api_token():
     secret = config("SOCIAL_AUTH_AUTH0_SECRET")
     base_api_url = config("AUTH0_BASE_API_URL")
     data = f"grant_type=client_credentials&client_id={id}&client_secret={secret}&audience={quote(base_api_url + '/api/v2/')}"
-    headers = {'content-type': "application/x-www-form-urlencoded"}
+    headers = {'content-type': "application/x-www-form-urlencoded",'Cache-Control': 'no-cache'}
     r = requests.post(f"{base_api_url}/oauth/token",data=data, headers=headers)
+    print(r.data())
     with open("/app/storage/cron_auth0_api_token", "w") as f:
         f.write(r.json()["access_token"])
 if __name__ == "__main__":
--- socialapp/middleware/email_verification_middleware.py
+++ socialapp/middleware/email_verification_middleware.py
@@ -12,6 +12,7 @@ class EmailVerificationMiddleware:
         self.get_response = get_response
     
     def __call__(self, request):
+        """
         # should only users with an account be allowed to view site?
         if not request.user.is_anonymous and not request.user.is_staff:
             if not request.user.is_verified and request.path not in ["/verification/", "/logout/"]: #and (not request.user.is_anonymous): # and (request.path != "/verification/"
@@ -25,5 +26,6 @@ class EmailVerificationMiddleware:
                     user.save()
                 else:
                     return HttpResponseRedirect("/verification/")
+        """
         response = self.get_response(request)
         return response
